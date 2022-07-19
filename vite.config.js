b990f131ac73444d73c602fca2aa6c0413b2471b
santiagovrancovich <santiagovrancovich182@gmail.com>
2022-07-19 16:44:34 -0300

    Minor Vite config change on dist folder location

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   root: "./",
-  publicDir: "../public",
+  publicDir: "./public",
   build: {
-    outDir: "../dist",
+    outDir: "./dist",
   },
   server: {
     port: 8080,
