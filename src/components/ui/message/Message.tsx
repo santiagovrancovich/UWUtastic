59e096855abb5b842710b7feedce01ea306e1b87
santiagovrancovich <santiagovrancovich182@gmail.com>
2022-07-20 12:28:17 -0300

    Replaced message typewriter animation with a bouncing animation

--- src/components/ui/message/Message.tsx
+++ src/components/ui/message/Message.tsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState, useLayoutEffect, useContext} from 'react'
 import { globalContext } from "../../../utils/context"
 import UWUsound from "../../../assets/uwu.mp3"
-import Typed from 'typed.js'
 import anime from 'animejs/lib/anime.es.js'
 import "./Message.scss"
 
@@ -16,26 +15,21 @@ export default function Message(props) {
         setmessageAnimation(anime({
             targets: message.current,
             opacity: [
-                {value:1, duration:500},
-                {value:0, duration:500, delay: props.sentence.length*60 },
+                {value:1, duration:1000},
+                {value:0, duration:1500, delay: props.sentence.length*60 },
+            ],
+            scaleX: [
+                {value: 0.65, duration: 0},
+                {value: 1, duration: 1000}
             ],
             autoplay: false,
-            easing: "linear",
+            easing: "easeOutElastic",
             begin: () => { 
                 context.dispatch({type:"pauseClick"}),
                 sound.paused ? sound.play() : sound.currentTime = 0
             } ,
             complete: () => context.dispatch({type:"pauseClick"})
         }))
-        
-        const options = {
-            strings: [props.sentence],
-            typeSpeed: 20
-        }
-        let typed = new Typed(message.current, options)
-        return () => {
-            typed.destroy()
-        }
     }, [props.refresh])
 
     useLayoutEffect(() => {
@@ -43,6 +37,6 @@ export default function Message(props) {
     }, [props.display])
 
     return (
-        <div className="text-container" ref={message}></div>
+        <div className="text-container" ref={message}>{ props.sentence }</div>
     )
 }
